e975cef15faaf345972125c508057d6bca8a5c95
Adilet Toloshov <toloshov_a@auca.kg>
2025-01-23 17:23:20 +0600

    Add lab5 folder with files

---
+++ lab5/labAssignment1.py
@@ -0,0 +1,26 @@
+name = "Adilet"
+print(f"My name is: {name}")
+
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+
+a = 3
+b = 7
+
+a, b = b, a
+print(f"a: {a}, b: {b}")
+
+PI = 3.14159
+radius = 5
+area = PI * radius**2
+print("Area f circle is:", area)
---
+++ lab5/labAssignment2.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello, my name is {name} and I am {age} years old.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+sum_of_numbers = num1 + num2 + num3
+average = sum_of_numbers / 3
+product = num1 * num2 * num3
+print(f"Sum: {sum_of_numbers}")
+print(f"Average: {average}")
+print(f"Product: {product}")
