1740f47943586c8c819cd0d6b955d60fd3bf94ff
Adilet Toloshov <toloshov_a@auca.kg>
2025-01-23 16:12:35 +0600

    Add lab4 folder with files

---
+++ lab4/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route("/hello/<name>")
+def hello_home(name):
+    return render_template("hello.html", name=name)
+        
+if __name__ == "__main__":
+    app.run(debug=True)
+
